49d240ca0c3f1d4ce617bfd18de5a5eef02fdf36
jundragons <yongkari9@gmail.com>
2022-02-25 17:28:11 +0900

    Add Categories model

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
+# User Model
 class User(models.Model):
     user_id = models.AutoField(primary_key = True)
     id = models.CharField(max_length=20)
@@ -11,3 +10,11 @@ class User(models.Model):
 
     def __str__(self):
         return self.id + self.nickname
+
+# Categories Model
+class Categories(models.Model):
+    category_id = models.AutoField(primary_key = True)
+    category = models.CharField(max_length=20)
+    user_id = models.ForeignKey('blog.User',on_delete=models.CASCADE,
+                                related_name='categories')
+    
