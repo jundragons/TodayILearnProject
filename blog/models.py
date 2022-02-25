6a263f536e8f48ee91c2aae152661fbb2593f07e
jundragons <yongkari9@gmail.com>
2022-02-25 17:21:34 +0900

    Add User Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key = True)
+    id = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.id + self.nickname
