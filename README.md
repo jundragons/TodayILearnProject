42c64bee35c946f11edf61fbc9c20423037f5cb0
jundragons <yongkari9@gmail.com>
2022-02-25 18:10:22 +0900

    Add post model

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TodayILearnProject
 Make Today I Learn application for Django framework
+
+
+
+# ERD
+
+![image-20220225173743850](C:\Users\Junyong\AppData\Roaming\Typora\typora-user-images\image-20220225173743850.png)
--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     user_id = models.AutoField(primary_key = True)
     id = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.id + self.nickname
@@ -15,6 +15,17 @@ class User(models.Model):
 class Categories(models.Model):
     category_id = models.AutoField(primary_key = True)
     category = models.CharField(max_length=20)
-    user_id = models.ForeignKey('blog.User',on_delete=models.CASCADE,
+    user_id = models.ForeignKey('blog.User', on_delete=models.CASCADE,
                                 related_name='categories')
-    
+
+# Post Model
+class Post(models.Model):
+    post_id = models.AutoField(primary_key = True)
+    user_id = models.ForeignKey('blog.User', on_delete=models.CASCADE,
+                                related_name='post')
+    category_id = models.ForeignKey('blog.Categories', on_delete=models.CASCADE,
+                                    related_name='post')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
