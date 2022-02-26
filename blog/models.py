94ade71e56a7e6d20555b5f9f40d462fe4ee8818
jundragons <yongkari9@gmail.com>
2022-02-26 20:17:24 +0900

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,8 @@ class User(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.id + self.nickname
+        return 'id: ' + self.id + \
+               ' 닉네임:' + self.nickname
 
 # Categories Model
 class Categories(models.Model):
@@ -16,7 +17,10 @@ class Categories(models.Model):
     category = models.CharField(max_length=20)
     user_id = models.ForeignKey('blog.User', on_delete=models.CASCADE,
                                 related_name='categories')
-
+    def __str__(self):
+        return 'category_id: ' + self.category + \
+               ' 소유자:' + self.user_id +\
+               ' 카테고리: ' + self.category
 # Post Model
 class Post(models.Model):
     post_id = models.AutoField(primary_key = True)
@@ -28,6 +32,13 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'post_id: ' + self.post_id + \
+               ' user_id: ' + self.user_id + \
+               ' title: ' + self.title + \
+               ' category ' + self.category_id + \
+               ' content ' + self.content
 
 # Comment Model
 class Comment(models.Model):
