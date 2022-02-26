1132324e453df214ede6ac1287fe950841f20048
jundragons <yongkari9@gmail.com>
2022-02-27 03:55:48 +0900

    Add UserForm class

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Categories, Post
+from django.contrib.auth.forms import UserCreationForm
+from .models import  Post, User
 
 
 class PostForm(forms.ModelForm):
@@ -11,3 +12,10 @@ class PostForm(forms.ModelForm):
             'content': '내용',
             'category': '카테고리'
         }
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(label="이메일")
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "email")
