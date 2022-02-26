78dccce65f7f3874f56c66dc423aca74ec707153
jundragons <yongkari9@gmail.com>
2022-02-27 03:18:45 +0900

    Modify __str__ in class Categories

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # User Model
 class User(models.Model):
     user_id = models.AutoField(primary_key = True)
@@ -8,25 +9,26 @@ class User(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return 'id: ' + self.id + \
-               ' 닉네임:' + self.nickname
+        return self.nickname +'(' + str(self.id) + ')'
+
 
 # Categories Model
 class Categories(models.Model):
     category_id = models.AutoField(primary_key = True)
     category = models.CharField(max_length=20)
-    user_id = models.ForeignKey('blog.User', on_delete=models.CASCADE,
+    user = models.ForeignKey('blog.User', on_delete=models.CASCADE,
                                 related_name='categories')
+
     def __str__(self):
-        return 'category_id: ' + str(self.category_id) + \
-               ' / 카테고리: ' + self.category + \
-               ' / user: ' + str(self.user_id)
+        return self.category
+
+
 # Post Model
 class Post(models.Model):
     post_id = models.AutoField(primary_key = True)
-    user_id = models.ForeignKey('blog.User', on_delete=models.CASCADE,
+    user = models.ForeignKey('blog.User', on_delete=models.CASCADE,
                                 related_name='post')
-    category_id = models.ForeignKey('blog.Categories', on_delete=models.CASCADE,
+    category = models.ForeignKey('blog.Categories', on_delete=models.CASCADE,
                                     related_name='post')
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -34,24 +36,19 @@ class Post(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return 'post_id: ' + str(self.post_id) + \
-               ' user_id: ' + str(self.user_id) + \
-               ' title: ' + self.title + \
-               ' category ' + str(self.category_id) + \
-               ' content ' + str(self.content)
+        return self.user.nickname + ' : ' + self.title
+
 
 # Comment Model
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key = True)
-    user_id = models.ForeignKey('blog.User', on_delete=models.CASCADE,
+    user = models.ForeignKey('blog.User', on_delete=models.CASCADE,
                                 related_name='comment')
-    post_id = models.ForeignKey('blog.Post', on_delete=models.CASCADE,
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE,
                                 related_name='comment')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return 'comment_id: ' + str(self.comment_id) + \
-               ' / post_id: ' + str(self.post_id) + \
-               ' / content: ' + self.content
+        return self.content + ' 글번호 :' + str(self.post.post_id)
