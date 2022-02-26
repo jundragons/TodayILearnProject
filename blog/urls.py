b9b56597e652bbcf8dcf4056c78a11dca0ea6f99
jundragons <yongkari9@gmail.com>
2022-02-26 23:25:21 +0900

    Add post_create url

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    # http://localhost:8080 (not login)
+    path('', views.index, name='index'),
+    # http://localhost:8080 (login)
+    path('post/<int:user_id>/', views.post_list, name='post_list'),
+    # http://localhost:8080/post/<int:user_id>/create/
+    path('post/<int:user_id>/create/', views.post_create, name='post_create')
 ]
