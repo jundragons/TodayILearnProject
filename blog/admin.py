8a8fe9882ec98c746a6c30ac027a4bbae27a213a
jundragons <yongkari9@gmail.com>
2022-02-26 20:16:47 +0900

    Add categoires, comment for admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Post
 from .models import User
-
+from .models import Categories
+from .models import Comment
 
 admin.site.register(Post)
 admin.site.register(User)
+admin.site.register(Categories)
+admin.site.register(Comment)
