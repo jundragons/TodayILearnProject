266ed8e18cdb67bac3ddbc0915680a193fa8051e
jundragons <yongkari9@gmail.com>
2022-02-26 23:25:01 +0900

    Add post_create method

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
+# from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import User, Post
+from .forms import PostForm
+
+def index(request):
+    user_list = User.objects.all()
+    return render(request, 'blog/index.html', {'users' : user_list})
 
 
-def post_list(request):
-    return render(request, 'blog/post_list.html')
+def post_list(request, user_id):
+    posts = Post.objects.filter(user_id = user_id).order_by('-created_at')
+    return render(request, 'blog/post_list.html',
+                  {'posts' : posts})
+
+
+def post_create(request):
+    form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
