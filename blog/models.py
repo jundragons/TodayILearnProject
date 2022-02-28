36c14e68095018ede97b705d3c425509b727020e
jundragons <yongkari9@gmail.com>
2022-02-28 21:11:06 +0900

    Delete User Model

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,12 @@
 from django.db import models
-
-
-# User Model
-class User(models.Model):
-    user_id = models.AutoField(primary_key = True)
-    id = models.CharField(max_length=20)
-    nickname = models.CharField(max_length=20)
-    created_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.nickname +'(' + str(self.id) + ')'
+from django.contrib.auth.models import User
 
 
 # Categories Model
 class Categories(models.Model):
     category_id = models.AutoField(primary_key = True)
     category = models.CharField(max_length=20)
-    user = models.ForeignKey('blog.User', on_delete=models.CASCADE,
-                                related_name='categories')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
 
     def __str__(self):
         return self.category
@@ -26,8 +15,7 @@ class Categories(models.Model):
 # Post Model
 class Post(models.Model):
     post_id = models.AutoField(primary_key = True)
-    user = models.ForeignKey('blog.User', on_delete=models.CASCADE,
-                                related_name='post')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey('blog.Categories', on_delete=models.CASCADE,
                                     related_name='post')
     title = models.CharField(max_length=200)
@@ -36,14 +24,13 @@ class Post(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.nickname + ' : ' + self.title
+        return self.user.username + ' : ' + self.title
 
 
 # Comment Model
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key = True)
-    user = models.ForeignKey('blog.User', on_delete=models.CASCADE,
-                                related_name='comment')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE,
                                 related_name='comment')
     content = models.TextField()
