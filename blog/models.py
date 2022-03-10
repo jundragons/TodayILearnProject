69a64365d50f8c227f8b55e897888f66fea45405
jundragons <yongkari9@gmail.com>
2022-03-10 23:24:44 +0900

    Modify Categories model user on_delete

--- blog/models.py
+++ blog/models.py
@@ -14,10 +14,10 @@ class Categories(models.Model):
 
 # Post Model
 class Post(models.Model):
-    post_id = models.AutoField(primary_key = True)
+    post_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey('blog.Categories', on_delete=models.CASCADE,
-                                    related_name='post')
+    category = models.ForeignKey('blog.Categories', on_delete=models.SET_NULL, null=True,
+                                 related_name='post')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -32,7 +32,7 @@ class Comment(models.Model):
     comment_id = models.AutoField(primary_key = True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE,
-                                related_name='comment')
+                             related_name='comment')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
