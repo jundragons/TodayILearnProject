a689c7aa0f51cf044b3feb5f52e4c4712a2fce37
jundragons <yongkari9@gmail.com>
2022-04-15 17:54:15 +0900

    modify settings.py

--- todayIlearn/settings.py
+++ todayIlearn/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
+import pymysql
+# pymysql django ORM 연동
+pymysql.install_as_MySQLdb()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
